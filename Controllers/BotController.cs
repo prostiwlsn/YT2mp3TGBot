07ecb9328bb312e8e1f14cf45d088925e428f27d
prostiwlsn <prostiwlsn@gmail.com>
2024-03-20 00:28:48 +0700

    webhook tested

---
+++ Controllers/BotController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace YT2mp3.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class BotController : ControllerBase
+    {
+        private readonly ILogger<BotController> _logger;
+
+        public BotController(ILogger<BotController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update([FromBody] Object update)
+        {
+            // Handle the update here
+            Console.WriteLine("webhook working as intended");
+            return Ok();
+        }
+    }
+}
