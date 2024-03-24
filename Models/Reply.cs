ac0bd95aadbed2b2bbc6e80c0341a2ded1df0782
prostiwlsn <prostiwlsn@gmail.com>
2024-03-24 21:21:12 +0700

    Model cleanup

--- Models/Reply.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace YT2mp3.Models
-{
-    public class Reply
-    {
-        public int chat_id {  get; set; }
-        public string text { get; set; }
-    }
-}
--- Models/Update.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace YT2mp3.Models
-{
-    public class Update
-    {
-        public int update_id {  get; set; }
-        public Message message { get; set; }
-    }
-}
